bfb08653998ba312eeb7b37927e70ccd5d4108c1
kushan <kushansuk2@gmail.com>
2022-12-10 21:39:50 +0530

    added config file with api key

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "c3066788826c1323bdc23cf78b91be61"
+    }
+}
+
+module.exports = constants;
